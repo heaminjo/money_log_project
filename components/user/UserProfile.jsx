5ff63b81e7afaa8b9e9b8464f5416256901619cc
heaminjo <157016835+heaminjo@users.noreply.github.com>
2025-03-25 12:35:21 +0900

    Add files via upload

---
+++ components/user/UserProfile.jsx
@@ -0,0 +1,9 @@
+import UserProfileComp from "./UserProfileStyle";
+
+export default function UserProfile() {
+  return (
+    <UserProfileComp>
+      <h1>유저 프로필</h1>
+    </UserProfileComp>
+  );
+}
